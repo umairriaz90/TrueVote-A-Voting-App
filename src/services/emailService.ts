83ab1215ba9da89df684a9c5ce8666f466f0826f
umairriaz90 <umairriaz90@gmail.com>
2024-12-07 16:58:40 +0500

    newone

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_5Dx4HRVG_Crg5x1jCJoANaw4aW7DL7RsB");
 
 export const sendVotingLink = async (email: string, votingLink: string) => {
   try {
